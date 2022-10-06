6aa1a6b1982f994fa8cad09941f7d76ad8febbf9
spriyadarshini-bex <97087466+spriyadarshini-bex@users.noreply.github.com>
2022-10-06 19:57:48 +0530

    HectoberFest

---
+++ script.js
@@ -0,0 +1,11 @@
+//log message
+console.log('This is a log message');
+
+//Information message
+console.info('This is an information message');
+
+//Warning message
+console.warn('This is a warning message');
+
+//Error message
+console.error('This is an error message');
